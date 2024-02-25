c2056a60d6141b3abab54421fee9f99da9efdf3b
nichtmehr99 <nichtmehr99@gmail.com>
2024-02-25 09:24:32 +0100

    ar akta module baki and HW

---
+++ quiz.js
@@ -0,0 +1,35 @@
+function min(nums) {
+  return Math.min(nums);
+}
+// console.log(min([1, 3, 2]));
+// const nums =[1,2,34]
+// console.log (min(nums))
+const cube = (n) => n * n * n;
+// console.log(cube(3))
+
+const [a, b] = [1, 2, 3, 4, 45, 5];
+// const valueA = console.log(a)
+// const valueB = console.log(b)
+// const result = valueA + valueB
+// console.log(a+b)
+const { x, y, z, job } = {
+  x: 1,
+  y1: 2,
+  z: 3,
+  job: "technisian",
+};
+
+// console.log(work)
+
+const nums = [1, 2, 3, 4, 5];
+let output = nums.filter((n) => n % 2 === 1);
+console.log(output);
+
+const friends = ["Moushumi", "Misha", "Manna", "mimi", "mahiya"];
+const friend5 = friends.find(frnd => frnd.length === 5)
+// console.log(friend5)
+const product = {name: 'Laptop', model:'Yoga 3', price:49000, dusk: '512SSD'}
+// const product: new Product()
+
+
+
